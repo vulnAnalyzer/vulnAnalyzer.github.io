054de31ea8e14f199d91a0d297f25f8c50ec0398
vulnAnalyzer <vulnAnalyzer@outlook.com>
2025-08-28 18:57:51 +0800

    update

--- re-implementVAScanner/Tai-e/install.sh
+++ re-implementVAScanner/Tai-e/install.sh
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
-=======
-#!/bin/bash
->>>>>>> 29ceb37f3c2412b1b8c68ed17f73205aafb04611
+
  mvn install:install-file "-Dfile=Tai-e-plugin-1.0.jar" "-DgroupId=neu.lab" "-DartifactId=Tai-e-plugin" "-Dversion=1.0" "-Dpackaging=jar" "-DpomFile=Tai-e-plugin-1.0.pom"
